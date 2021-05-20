5fb600dd0793f944a4bcf5f4c5f934be32b53531
SZPIEGA Emmanuel <emmanuel.szpiega@sword-group.com>
2021-05-20 20:15:47 +0200

    First commit

---
+++ src/build/precache-manifest.99da208e5cd255122a0a20f6e8f26f34.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "79ae0db7d302c696e968",
+    "url": "/static/css/main.208d3124.chunk.css"
+  },
+  {
+    "revision": "79ae0db7d302c696e968",
+    "url": "/static/js/main.1462cbfa.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d72546995997ab1773b6",
+    "url": "/static/js/2.f96445f7.chunk.js"
+  },
+  {
+    "revision": "ccb14ab6c9efeacd0caab95cce77a1ad",
+    "url": "/index.html"
+  }
+];
